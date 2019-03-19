934dd431f2d8935be80ed86004f9ce7dd087d9bf
James Walmsley <james@fullfat-fs.co.uk>
2019-03-19 23:09:09 +0000

    [RECIPE] rpi3-toolchain - Implemented building of binutils.

--- recipes/rpi3-toolchain/layers/binutils.mk
+++ recipes/rpi3-toolchain/layers/binutils.mk
@@ -1,9 +1,35 @@
 LAYER:=binutils
 include $(DEFINE_LAYER)
 
-#DEPENDS += toolchain-libs
+binutils:=$(LSTAMP)/binutils
+
+$(L) += $(binutils)
+
+DEPENDS += toolchain-libs
 
 $(call git_clone, binutils, https://github.com/bminor/binutils-gdb.git, binutils-2_29_1.1)
 
-
 include $(BUILD_LAYER)
+
+
+BINUTILS_CONFIG += --disable-nls
+BINUTILS_CONFIG += --enable-interwork
+BINUTILS_CONFIG += --disable-sim
+BINUTILS_CONFIG += --disable-gdb
+BINUTILS_CONFIG += --enable-plugins
+BINUTILS_CONFIG += --with-sysroot=$(SYSROOT)
+BINUTILS_CONFIG += --infodir=$(DOCROOT)/info
+BINUTILS_CONFIG += --mandir=$(DOCROOT)/man
+BINUTILS_CONFIG += --pdfdir=$(DOCROOT)/pdf
+
+$(binutils):
+	rm -rf $(builddir)/binutils
+	@mkdir -p $(builddir)/binutils
+	cd $(builddir)/binutils && $(ENVFLAGS) $(srcdir)/binutils/configure --host=$(TC_HOST) --build=$(TC_BUILD) --target=$(TC_TARGET) --prefix=$(TC_PREFIX) $(BINUTILS_CONFIG)
+	cd $(builddir)/binutils && $(MAKE)
+	cd $(builddir)/binutils && $(MAKE) install
+	$(stamp)
+
+binutils-docs: $(OUTDIR)/_binutils_build
+	cd $(builddir)/binutils && $(MAKE) install-html install-pdf
+	@touch $@
--- recipes/rpi3-toolchain/recipe.mk
+++ recipes/rpi3-toolchain/recipe.mk
@@ -8,6 +8,8 @@ hostlibs:=$(OUT)/host-libs
 
 TC_BUILD?=$(shell gcc -dumpmachine)
 TC_HOST?=$(shell $(TC_BUILD)-gcc -dumpmachine)
+TC_TARGET?=aarch64-linux-gnu
+TC_PREFIX?=$(OUT)/$(TC_HOST)/$(TC_TARGET)
 
 TC_HOSTENV:=CC=$(TC_HOST)-gcc CXX=$(TC_HOST) AR=ar RANLIB=$(TC_HOST)-ranlib STRIP=$(TC_HOST)-strip NM=$(TC_HOST)-nm AS=$(TC_HOST)-as OBJDUMP=$(TC_HOST)-objdump
 
