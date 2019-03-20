93b2d02aa3b43bd8aedbc86419cb8cf54f5b1c97
James Walmsley <james@fullfat-fs.co.uk>
2019-03-20 15:47:17 +0000

    [RECIPE] rpi3 - Updated binutils recipe.

--- recipes/rpi3-toolchain/layers/binutils.mk
+++ recipes/rpi3-toolchain/layers/binutils.mk
@@ -1,16 +1,19 @@
 LAYER:=binutils
 include $(DEFINE_LAYER)
 
+binutils-configure:=$(LSTAMP)/binutils-configure
 binutils:=$(LSTAMP)/binutils
+binutils-install:=$(LSTAMP)/binutils-install
 
+$(L) += $(binutils-configure)
 $(L) += $(binutils)
+$(L) += $(binutils-install)
 
 DEPENDS += toolchain-libs
 
 $(call git_clone, binutils, https://github.com/bminor/binutils-gdb.git, binutils-2_29_1.1)
 
 include $(BUILD_LAYER)
-
 
 BINUTILS_CONFIG += --disable-nls
 BINUTILS_CONFIG += --enable-interwork
@@ -22,13 +25,26 @@ BINUTILS_CONFIG += --infodir=$(DOCROOT)/info
 BINUTILS_CONFIG += --mandir=$(DOCROOT)/man
 BINUTILS_CONFIG += --pdfdir=$(DOCROOT)/pdf
 
-$(binutils):
+
+
+$(binutils-configure):
 	rm -rf $(builddir)/binutils
-	@mkdir -p $(builddir)/binutils
+	mkdir -p $(builddir)/binutils
 	cd $(builddir)/binutils && $(ENVFLAGS) $(srcdir)/binutils/configure --host=$(TC_HOST) --build=$(TC_BUILD) --target=$(TC_TARGET) --prefix=$(TC_PREFIX) $(BINUTILS_CONFIG)
+	$(stamp)
+
+
+$(binutils):
 	cd $(builddir)/binutils && $(MAKE)
+	$(stamp)
+
+$(binutils): $(binutils-configure)
+
+$(binutils-install):
 	cd $(builddir)/binutils && $(MAKE) install
 	$(stamp)
+
+$(binutils-install): $(binutils)
 
 binutils-docs: $(OUTDIR)/_binutils_build
 	cd $(builddir)/binutils && $(MAKE) install-html install-pdf
