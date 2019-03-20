0656e4947eb89b5f6c62849af4f8d2bd7e8ecc30
James Walmsley <james@fullfat-fs.co.uk>
2019-03-20 15:48:37 +0000

    [RECIPE] rpi3-toolchain - Updated tarball layer.

--- recipes/rpi3-toolchain/layers/tarball.mk
+++ recipes/rpi3-toolchain/layers/tarball.mk
@@ -3,7 +3,7 @@ LAYER:=tarball
 include $(DEFINE_LAYER)
 
 tarball:=$(BUILD_tarball)/$(TC_HOST)-$(TC_TARGET).tar
-7z:=$(OUT)/$(TC_HOST)-$(TC_TARGET).7z
+7z:=$(OUT)/archives/$(TC_HOST)-$(TC_TARGET).7z
 
 $(L) += $(tarball)
 $(L) += $(7z)
@@ -15,6 +15,7 @@ $(tarball):
 	tar cvf $@ -C $(OUT)/$(TC_HOST)/$(TC_TARGET) .
 
 $(7z):
+	mkdir -p $(dir $@)
 	7z a $@ -mx9 -mmt -md29 -ms=on -myx=9 -mfb=273 $(tarball)
 
 $(7z): $(tarball)
