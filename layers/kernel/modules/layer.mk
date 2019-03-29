16e8c843e5339849a022085255c048f5c4070998
James Walmsley <james@fullfat-fs.co.uk>
2019-03-29 16:23:10 +0000

    [LAYERS] Refactored kernel-modules layer.

---
+++ layers/kernel/modules/layer.mk
@@ -0,0 +1,17 @@
+LAYER:=kernel-modules
+include $(DEFINE_LAYER)
+
+kernel-modules:=$(LSTAMP)/modules
+
+$(L) += $(kernel-modules)
+
+DEPENDS += debian-provision
+DEPENDS += kernel
+
+include $(BUILD_LAYER)
+
+$(kernel-modules):
+	rm -rf $(ROOTFS)/lib/modules
+	cd $(KERNEL_SOURCE) && $(MAKE) O=$(KERNEL_OUT) ARCH=$(ARCH) modules_install INSTALL_MOD_PATH=$(ROOTFS)
+	$(stamp)
+
