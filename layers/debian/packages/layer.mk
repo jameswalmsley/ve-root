bb924b67b3acd6b5f012c330bbe346466a6e5104
James Walmsley <james@fullfat-fs.co.uk>
2019-04-01 16:21:19 +0100

    [DEBIAN] Install packages from TOP and RECIPE (dev- and packages.list).

--- layers/debian/packages/layer.mk
+++ layers/debian/packages/layer.mk
@@ -13,8 +13,17 @@ $(debian-install-packages):
 	$(QEMU_START)
 	rsync -av $(basedir)/scripts/install-packages.sh $(ROOTFS)/install-packages.sh
 	rsync -av $(RECIPE)/packages.list $(ROOTFS)/packages.list
+ifneq ($(wildcard $(RECIPE)/packages.list),)
+	cat $(RECIPE)/packages.list >> $(ROOTFS)/packages.list
+endif
 ifneq ($(wildcard $(TOP)/packages.list),)
+	cat $(RECIPE)/dev-packages.list >> $(ROOTFS)/packages.list
+endif
+ifneq ($(wildcard $(RECIPE)/dev-packages.list),)
 	cat $(TOP)/packages.list >> $(ROOTFS)/packages.list
+endif
+ifneq ($(wildcard $(TOP)/dev-packages.list),)
+	cat $(TOP)/dev-packages.list >> $(ROOTFS)/packages.list
 endif
 	chroot $(ROOTFS) bash -c /install-packages.sh
 	rm $(ROOTFS)/packages.list $(ROOTFS)/install-packages.sh
@@ -23,7 +32,22 @@ endif
 
 $(debian-install-packages): $(BASE_debian-packages)/scripts/install-packages.sh
 $(debian-install-packages): | $(debian-full-upgrade)
+
+ifneq ($(wildcard $(RECIPE)/packages.list),)
 $(debian-install-packages): $(RECIPE)/packages.list
+endif
+
+ifneq ($(wildcard $(RECIPE)/dev-packages.list),)
+$(debian-install-packages): $(RECIPE)/dev-packages.list
+endif
+
+ifneq ($(wildcard $(TOP)/packages.list),)
+$(debian-install-packages): $(TOP)/packages.list
+endif
+
+ifneq ($(wildcard $(TOP)/dev-packages.list),)
+$(debian-install-packages): $(TOP)/dev-packages.list
+endif
 
 ifneq ($(wildcard $(TOP)/packages.list),)
 $(debian-install-packages): $(TOP)/packages.list
