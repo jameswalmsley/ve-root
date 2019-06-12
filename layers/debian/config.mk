d841bbdc94d298ae1324f6935d8f81a8ccb3fa41
James Walmsley <james@fullfat-fs.co.uk>
2019-06-12 13:11:53 +0100

    [DEBIAN] Added DEBIAN_ARCH_TRIPLET.

--- layers/debian/config.mk
+++ layers/debian/config.mk
@@ -2,6 +2,10 @@ DEBIAN_PATCH:=$(BASE)/layers/debian/.scripts/rootfspatch
 DEBIAN_PATCH_FILE?=config.json
 DEBIAN_PATCH_CONFIG:=$(call select_file,$(TOP)/$(DEBIAN_PATCH_FILE),$(RECIPE)/$(DEBIAN_PATCH_FILE))
 
+ifeq ($(RECIPE_DEB_ARCH),arm64)
+DEBIAN_ARCH_TRIPLET:=aarch64-linux-gnu
+endif
+
 DEBIAN_PACKAGES += apt-utils
 DEBIAN_PACKAGES += kmod
 DEBIAN_PACKAGES += pkg-config
