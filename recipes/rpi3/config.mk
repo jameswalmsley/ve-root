12c3f614f1a96b5e2705aaefb92f9c5e6421f0b0
James Walmsley <james@fullfat-fs.co.uk>
2019-07-04 23:00:50 +0100

    [RECIPE] rpi3 - QEMU_START and QEMU_DONE - Defined in debian config.mk

--- recipes/rpi3/config.mk
+++ recipes/rpi3/config.mk
@@ -5,5 +5,3 @@ RECIPE_DEB_ARCH:=$(ARCH)
 RECIPE_DEB_RELEASE:=bionic
 BOOT_IMAGE_SIZE:=48M
 
-QEMU_START:=cp /usr/bin/qemu-aarch64-static $(ROOTFS)/usr/bin
-QEMU_DONE:=rm $(ROOTFS)/usr/bin/qemu-aarch64-static
