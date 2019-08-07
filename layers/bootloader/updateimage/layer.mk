e4a101a54b581e79b5ee69c6066a448fc5e1619b
James Walmsley <james@fullfat-fs.co.uk>
2019-08-07 13:14:52 +0100

    [BOOTLOADER] updateimage - Fix generation based on SECURE_BOOT etc.

--- layers/bootloader/updateimage/layer.mk
+++ layers/bootloader/updateimage/layer.mk
@@ -1,7 +1,13 @@
 LAYER:=bootloader-updateimage
 include $(DEFINE_LAYER)
 
-bootloader-updateimage.fit:=$(BUILD)/$(L)/updateimage.fit
+bootloader-updateimage-suffix:=
+
+ifneq ($(CONFIG_SECURE_BOOT),y)
+bootloader-updateimage-suffix:=$(strip $(bootloader-updateimage-suffix)).signed
+endif
+
+bootloader-updateimage.fit:=$(BUILD)/$(L)/updateimage$(bootloader-updateimage-suffix).fit
 bootloader-updateimage.its:=$(BUILD)/$(L)/updateimage.its
 
 $(L) += $(bootloader-updateimage.fit)
@@ -30,7 +36,11 @@ $(bootloader-updateimage.fit):
 	cd $(UPDATEIMAGE_OUT) && dtc -p 0x1000 -I dtb -O dtb dtree.dtb -o devicetree.dtb
 	cd $(UPDATEIMAGE_OUT) && fdtput -ts devicetree.dtb "/chosen" "bootargs" "$(shell cat $(bootloader-bootargs))"
 	cp $(bootloader-updateimage.its) $(UPDATEIMAGE_OUT)
+ifneq ($(CONFIG_SECURE_BOOT),y)
 	cd $(UPDATEIMAGE_OUT) && $(MKIMAGE) -D "-I dts -O dtb -p 0x1000" -f updateimage.its $@
+else
+	cd $(UPDATEIMAGE_OUT) && $(MKIMAGE) -D "-I dts -O dtb -p 0x1000" -f updateimage.its -k $(KEY_PATH_ABS) $@
+endif
 
 
 $(bootloader-updateimage.fit): $(bootloader-updateimage.its)
@@ -38,3 +48,5 @@ $(bootloader-updateimage.fit): $(bootloader-bootargs)
 
 $(bootloader-updateimage.its):
 	python3 $(DEBIAN_PATCH)/generate.py $(BASE_bootloader-updateimage)/updateimage $(dir $(bootloader-updateimage.its)) $(DEBIAN_PATCH_CONFIG)
+
+$(bootloader-updateimage.its): $(BASE_bootloader-updateimage)/updateimage/updateimage.its
