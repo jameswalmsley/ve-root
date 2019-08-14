6a2789ad14801efe4128b773ebac526bb40a0202
James Walmsley <james@fullfat-fs.co.uk>
2019-08-14 15:05:28 +0100

    [DEBIAN] bootconfig - migrated network configuration improvements.

--- layers/debian/bootramfs/ramdisk/bootconfig.sh
+++ layers/debian/bootramfs/ramdisk/bootconfig.sh
@@ -1,3 +1,5 @@
+ERROR_IP_ADDRESS="172.24.0.1/24"
+
 config_network_dhcp_server()
 {
     ln -s wired.network.dhcp_server wired.network
@@ -8,17 +10,27 @@ config_network_dhcp_client()
     ln -s wired.network.dhcp wired.network
 }
 
-config_network_static()
+_config_network_static()
 {
     ln -s wired.network.static wired.network
-    ip=$(cat $rootdir/data/network.config | grep -E '(([0-9]{1,3})\.){3}([0-9]{1,3}){1}' | grep -vE '25[6-9]|2[6-9][0-9]|[3-9][0-9][0-9]')
+    ip=$1
     if [ ! "$ip" ]; then
-        rm wired.network
-        config_network_dhcp_server
-        return
+        ip=$ERROR_IP_ADDRESS
     fi
     # Replace the ip field with correct.
     sed -i "s;Address=.*;Address=${ip};g" wired.network
+}
+
+config_network_error()
+{
+    _config_network_static $ERROR_IP_ADDRESS
+}
+
+config_network_static_ip()
+{
+    cat $rootdir/data/network.config
+    ip=$(cat $rootdir/data/network.config | grep -E '^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\/([0-9]|[1-2][0-9]|3[0-2])$')
+    _config_network_static $ip
 }
 
 config_network()
@@ -37,6 +49,7 @@ config_network()
         if [ $? -eq 0 ]; then
             network_mode=$mode
         else
+            network_mode="--error--"
             echo "Syntax error in $rootdir/data/network.config"
             echo "Config mode must be: dhcp_server | dhcp_client | static"
             echo "NOTE:: No space before or after mode"
@@ -64,6 +77,7 @@ config_network()
 
         *)
             echo "Syntax or corrupt network.config file"
+            config_network_error
             ;;
     esac
 
