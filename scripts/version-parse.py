5af8cfde7abed331a53066a86d7c45fb9ac26d74
James Walmsley <james@fullfat-fs.co.uk>
2020-01-01 17:13:25 +0000

    [SCRIPTS] Added version-parse.py scripts based on semantic versioning.

---
+++ scripts/version-parse.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from packaging import version
+import sys
+
+if len(sys.argv) == 3:
+
+    a = version.parse(sys.argv[1])
+    b = version.parse(sys.argv[2])
+
+    if a == b:
+        print("eq")
+        sys.exit(0)
+
+    if a < b:
+        print("lt")
+        sys.exit(-1)
+
+    if a > b:
+        print("gt")
+        sys.exit(1)
+
+if len(sys.argv) == 4:
+
+    a = version.parse(sys.argv[2])
+    b = version.parse(sys.argv[3])
+
+    if sys.argv[1] == "--gteq":
+        if a >= b:
+            print("y")
+            sys.exit(0)
+        else:
+            print("n")
+            sys.exit(1)
+
+
