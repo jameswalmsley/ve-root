4c63e171800eba495432fc728154db44d050a02e
James Walmsley <jamesw@graphcore.ai>
2022-03-15 14:43:06 +0000

    [SWAY] Improve libinput build.

--- recipes/sway/layers/libinput.mk
+++ recipes/sway/layers/libinput.mk
@@ -12,12 +12,13 @@ DEB_PACKAGES += check
 $(call git_clone, libinput, https://github.com/wayland-project/libinput.git, $(libinput_GIT_REF))
 
 DEPENDS += wayland
+DEPENDS += wayland-protocols
 
 include $(BUILD_LAYER)
 
 $(libinput):
 	mkdir -p $(builddir)/libinput
-	cd $(builddir)/libinput && meson $(MESON_OPTIONS) $(srcdir)/libinput $(builddir)/libinput
+	cd $(builddir)/libinput && meson $(MESON_OPTIONS) $(srcdir)/libinput $(builddir)/libinput -Ddebug-gui=false -Dtests=false
 	cd $(builddir)/libinput && ninja
 	cd $(builddir)/libinput && $(SUDO) DESTDIR=$(SYSROOT) ninja install
 	cd $(builddir)/libinput && $(SUDO) ninja install
