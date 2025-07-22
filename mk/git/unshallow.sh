20bd217ac9152a3ddf1b09ab55d8b2f02433c3b6
James Walmsley <james.walmsley@chargepoint.com>
2025-07-22 11:09:11 +0000

    git: unshallow should give some output

--- mk/git/unshallow.sh
+++ mk/git/unshallow.sh
@@ -4,6 +4,6 @@ function="unshallow"
 
 . ${VEBASE}/mk/git/git.sh
 
-git fetch --unshallow 2> /dev/null || true
+git fetch --unshallow || true
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 git fetch
